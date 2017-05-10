df89c01a9d9aa3aa84b698569d45405a236c6805
Mike Tikh <Mike@Mikes-MacBook-Pro.local>
2017-05-10 08:43:29 -0700

    Added interactive pins to map

--- js/helper.js
+++ js/helper.js
@@ -86,7 +86,7 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  logClicks(loc.clientX, loc.clientY);
 });
 
 
@@ -178,6 +178,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map,marker);
     });
 
     // this is where the pin actually gets added to the map.
